537c3870f50b4f5203f0691ac835691fb3eae66d
matukai <matt.tanji@gmail.com>
2018-02-07 11:07:32 -1000

    users: GET & POST working

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,32 @@
 //MODULES
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex/knex');
+
+
+router
 
 
 
 
+
+
+
+
+.post('/new', (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let inventory = req.body.inventory;
+  let price = req.body.price;
+  console.log(price)
+  knex.raw(`INSERT INTO products (title, description, inventory, price) VALUES (?,?,?,?) RETURNING *`, [title, description, inventory, price])
+  .then((result) => {
+    res.json(result.rows[0])
+  })
+  .catch(error => {
+    res.json({message: 'Product already exists'})
+  })
+});
 
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -4,27 +4,68 @@ const router = express.Router();
 const knex = require('../knex/knex');
 
 
-router
-//.get('/users/:user_id', function (req, res, next) {
-//  let id = req.params.id.user_id;
-//  knex.raw('SELECT * FROM users WHERE user.id = (?)',[id]);
+router.get('/:user_id', (req, res) => {
+  let id = req.params.user_id
+  return knex.raw('SELECT * FROM users WHERE id = ?', [id])
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      return res.status(400).json({
+        message: 'User not found'
+      });
+    })
+})
+
+router.post('/register', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if (!(email && password)) { //  OR !(email || password)
+    return res.status(400).json({
+      message: 'invalid credentials'
+    })
+  }
+  email = email.toLowerCase();
+  return knex.raw('SELECT users.email FROM users WHERE users.email = ?', [email])
+    .then(result => {
+      // if email exists
+      if (result.rows.length > 0) {
+        throw new Error('email exists')
+      } else {
+        return result;
+      }
+    })
+    .then(result => {
+      return knex.raw(`INSERT INTO users (email, password) VALUES (?,?)
+  RETURNING *`, [email, password]);
+    })
+    .then(result => {
+      res.json(result.rows[0]);
+    })
+    .catch(err => {
+      return res.status(400).json({
+        message: 'user already exists'
+      });
+    })
+})
+
+
+// .post('/login', (req, res) => {
+//   let email = req.body.email;
+//   let password = req.body.password;
+//   knex.raw(`SELECT * from users WHERE users.email = ?`, [email, password])
+//   .then((result) => {
+//     console.log(result);
+//   })
 // })
-
-
-  .post('/register', (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    console.log(email)
-    knex.raw(`INSERT INTO users (email, password) VALUES (?, ?) RETURNING *`, [email, password])
-    .then((result) => {
-      res.json(result.rows[0])
-    })
-    .catch(error => {
-      res.json({message: 'User already exists'});
-    })
-  });
-
 
 
 
 module.exports = router
+
+
+
+//you can have multiple knex.raw statements one knex.raw to sql if email exists
+//break everything down to very basic
+//knex.raw returns object that have properties that are rows
