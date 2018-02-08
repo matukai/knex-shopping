2fc1ad64393679012ec6928d00d57268b9ced192
matukai <matt.tanji@gmail.com>
2018-02-07 16:57:51 -1000

    products put working

--- routes/product.js
+++ routes/product.js
@@ -44,5 +44,51 @@ router.post('/new', (req, res) => {
     })
 });
 
+router.put('/:product_id', (req, res) => {
+  let {title, description, inventory, price} = req.body;
+  let id = req.params.product_id;
+  console.log(id);
+  if(!id){
+    return res.status(400).json({
+      message: 'Missing ID'
+    })
+  }
+  return knex.raw('SELECT * FROM products WHERE id = ?', [id])
+  .then(result => {
+    console.log(result.rows.length)
+    if(result.rows.length){
+      return result;
+    } else {
+      throw new Error('product id not found');
+    }
+  })
+  .then(result => {
+    return knex.raw('UPDATE products SET title = ?, description = ?, inventory = ?, price = ?', [title, description, inventory, price]);
+  })
+  .then(result => {
+    res.status(200).json(`message: Product_id: ${id} has been updated`);
+  })
+  .catch(err => {
+    return res.status(500).json({
+      message: err.message
+    })
+  })
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
 
 module.exports = router
